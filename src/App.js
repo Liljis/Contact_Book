e2bd26c3cb86e37a8e94dd8f2383d22ea98cf11c
Liljis <101180062+Liljis@users.noreply.github.com>
2022-10-13 16:24:04 +0530

    form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Favorites from './pages/Favorites';
+import NotFound from './pages/NotFound';
+import Nav from './components/Nav';
+import {useState} from 'react'
 
 function App() {
+  const [contacts, setcontacts] = useState([])
+  const formSub  = (data) => {
+    setcontacts([...contacts,data]);
+  }
+  // delete contact
+  const deleteContact = (id) =>{
+    let newContact = contacts.filter((singleContact) => {
+      return singleContact.id !== id;
+    })
+    setcontacts(newContact)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Nav/>
+    <Routes>
+      <Route path='/' element={<Home formSub={formSub} contacts={contacts} deleteContact={deleteContact}/>} />
+      <Route path='/Favorites' element={<Favorites/>} />
+      <Route path='*' element={<NotFound/>}/>
+    </Routes>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom'
+// like wise we have HashRouter which works similar to BrowserRouter but the url will not get changed there will be a # symbol after the main url
+// http://localhost:3000/#/Home
+// unstable_HistoryRouter this will keep the url history
+// MemoryRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
